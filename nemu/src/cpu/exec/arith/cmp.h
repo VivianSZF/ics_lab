d57be947cd474fe2bcdf2f46ad28a9cbd8004a9c
tracer-ics2016 <tracer@njuics.org>
2016-09-17 13:36:15 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:36:15 up  5:26,  1 user,  load average: 0.00, 0.01, 0.00 d214d02768fcf98890a9beb422dcb856dca124d

--- nemu/src/cpu/exec/arith/cmp.h
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -2,7 +2,7 @@
 #define __CMP_H__
 
 make_helper(cmp_i2a_b);
-make helper(cmp_i2rm_b);
+make_helper(cmp_i2rm_b);
 make_helper(cmp_si2rm_b);
 make_helper(cmp_r2rm_b);
 make_helper(cmp_rm2r_b);
@@ -10,7 +10,7 @@ make_helper(cmp_rm2r_b);
 make_helper(cmp_i2a_v);
 make_helper(cmp_i2rm_v);
 make_helper(cmp_si2rm_v);
-make_helepr(cmp_r2rm_v);
+make_helper(cmp_r2rm_v);
 make_helper(cmp_rm2r_v);
 
 #endif
