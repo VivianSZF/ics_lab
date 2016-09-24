b817e4490c70f214efb14aea46e700b6e84a33eb
tracer-ics2016 <tracer@njuics.org>
2016-09-24 16:16:28 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:16:28 up 38 min,  1 user,  load average: 0.05, 0.03, 0.00 66a725574c9b9461f2d11a118bfae7ea08b86a

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,20 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute(){
+	if(DATA_BYTE==2){
+		OPERAND_W(op_src,MEM_R(cpu.esp));
+		cpu.esp+=2;
+	}
+	else{
+		OPERAND_W(op_src,MEM_R(cpu.esp));
+		cpu.esp+=4;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(r)
+make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"
