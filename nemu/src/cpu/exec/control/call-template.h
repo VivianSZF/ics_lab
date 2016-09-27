93b7d1db55b46954433e491a8c2214d760b2c793
tracer-ics2016 <tracer@njuics.org>
2016-09-27 14:34:26 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  14:34:26 up  1:17,  1 user,  load average: 0.00, 0.00, 0.00 3eaa2de41a60166faa35ccbf48f221243c4de5d

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -6,11 +6,18 @@ static void do_execute(){
 	int len=instr_len();
 	cpu.esp-=4;
 	swaddr_write(cpu.esp,4,cpu.eip+len);
-	cpu.eip+=op_src->val;
-	print_asm("call %x",cpu.eip+len+1);
+	if(op_src->type==OP_TYPE_IMM){
+		cpu.eip+=op_src->val;
+		print_asm("call %x",cpu.eip+len+1);
+	}
+	else
+	{
+		cpu.eip=op_src->val-len-1;
+		print_asm_template1();
+	}
 }
 
 make_instr_helper(i);
-//make_instr_helper(rm);
+make_instr_helper(rm);
 
 #include "cpu/exec/template-end.h"
