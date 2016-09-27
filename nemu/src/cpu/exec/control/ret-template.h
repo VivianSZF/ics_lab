851b25233f802bff05761882929b1009009ac86f
tracer-ics2016 <tracer@njuics.org>
2016-09-27 15:15:21 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:15:21 up  1:58,  1 user,  load average: 0.00, 0.00, 0.00 8518eff54fc2519dc46a7cf5971869138ae1acd

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -4,21 +4,21 @@
 
 static void do_execute(){
 	if(DATA_BYTE==2){
-		cpu.eip=swaddr_read(cpu.esp,2)-1;
-		cpu.esp+=2;
+		cpu.eip=swaddr_read(cpu.esp,2);
+		cpu.esp+=2+op_src->val;
 		cpu.eip=cpu.eip&0xffff;
 	}
 	else
 	{
-		cpu.eip=swaddr_read(cpu.esp,4)-1;
-		cpu.esp+=4;
+		cpu.eip=swaddr_read(cpu.esp,4);
+		cpu.esp+=4+op_src->val;
 	}
-	if(ops_decoded.opcode==0xC2||ops_decoded.opcode==0xCA)
-		cpu.esp+=op_src->val;
 	if(op_src->val==0)
 		print_asm("ret");
-	else
+	else{
+		cpu.eip-=2;
 		print_asm_template1();
+	}
 }
 
 /*
