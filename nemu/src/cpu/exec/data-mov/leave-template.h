820740c567090f8e5d0a6704425eea5808c0d595
tracer-ics2016 <tracer@njuics.org>
2016-09-28 07:34:30 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  07:34:30 up  3:34,  1 user,  load average: 0.01, 0.02, 0.00 b427e06e2f8acc95651e924744aff0c8570596a3

---
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr leave
+
+static void do_execute(){
+	cpu.esp=cpu.ebp;
+	cpu.ebp=swaddr_read(cpu.esp,4);
+	cpu.esp+=4;
+	print_asm("leave");
+}
+
+make_instr_helper(n);
+
+#include"cpu/exec/template-end.h"
