83bbee84a61b769ba419eba548dc6d312a85520e
tracer-ics2016 <tracer@njuics.org>
2016-10-01 13:44:21 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:44:21 up  2:53,  1 user,  load average: 0.00, 0.00, 0.00 a519f78a73485d44ecc1283d902182649b25d67

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,7 +3,19 @@
 #define instr movsx
 
 static void do_execute(){
-	DATA_TYPE num=op_src->val;
+	DATA_TYPE num;
+	if(ops_decoded.opcode==0x0fbf)
+		num=(((DATA_TYPE_S)op_src->val)<<16)>>16;
+	else
+	{	
+		uint32_t t=(DATA_BYTE<<3)-8;
+		num=(((DATA_TYPE_S)op_src->val)<<t)>>t;
+	}
+	OPERAND_W(op_dest,num);
+	print_asm_template2();
 }
+
+make_instr_helper(rmb2r);
+make_instr_helper(rmw2r);
 
 #include "cpu/exec/template-end.h"
