5fcb8c607726874370dbc8db78db2f843b824860
tracer-ics2016 <tracer@njuics.org>
2016-10-02 06:21:02 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  06:21:02 up  1:05,  1 user,  load average: 0.00, 0.00, 0.00 58631ea78a1b6aacec794e13f4063f3e202d008

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,32 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute(){
+	DATA_TYPE result=op_src->val+op_dest->val;
+	OPERAND_W(op_dest,result);
+    DATA_TYPE pf=result;
+	pf=pf^(pf>>4);
+	pf=pf^(pf>>2);
+	pf=pf^(pf>>1);
+	pf=pf&1;
+	cpu.PF=!pf;
+	cpu.ZF=(result==0);
+	cpu.SF=MSB(result);
+	cpu.CF=(op_dest->val<op_src->val);
+	if((MSB(op_dest->val)!=MSB(op_src->val))&&(MSB(result)!=MSB(op_dest->val)))
+		cpu.OF=1;
+	else
+		cpu.OF=0;
+	print_asm_template2();
+}
+
+#if DATA_BYTE==2||DATA_BYTE==4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
