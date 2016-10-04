78ad9b1ffec655b4371fdf73fb8b4a77f2b6c602
tracer-ics2016 <tracer@njuics.org>
2016-10-04 04:42:19 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  04:42:19 up  1:04,  1 user,  load average: 0.13, 0.03, 0.01 e35a7432bcb0cb6a9207c5a6fa469cbb3eb50a8d

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -14,7 +14,7 @@ make_instr_helper(rm)
 #define instr setae
 static void do_execute(){
 	if(cpu.CF==0)
-		OPREAND_W(op_src,1);
+		OPERAND_W(op_src,1);
 	else
 		OPERAND_W(op_src,0);
 	print_asm_template1();
@@ -121,6 +121,94 @@ static void do_execute(){
 make_instr_helper(rm)
 #undef instr
 
+#define instr setnae
+static void do_execute(){
+	if(cpu.CF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnb
+static void do_execute(){
+	if(cpu.CF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnbe
+static void do_execute(){
+	if(cpu.CF==0&&cpu.ZF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnc
+static void do_execute(){
+	if(cpu.CF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setne
+static void do_execute(){
+	if(cpu.ZF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setng
+static void do_execute(){
+	if(cpu.ZF==1||cpu.SF!=cpu.OF)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnge
+static void do_execute(){
+	if(cpu.SF!=cpu.OF)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr_setnl
+static void do_execute(){
+	if(cpu.SF==cpu.OF)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
 #define instr setnle
 static void do_execute(){	
 	if(cpu.ZF==0||cpu.SF==cpu.OF)
@@ -132,3 +220,114 @@ static void do_execute(){
 make_instr_helper(rm)
 #undef instr
 
+#define instr setno
+static void do_execute(){
+	if(cpu.OF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnp
+static void do_execute(){
+	if(cpu.PF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setns
+static void do_execute(){
+	if(cpu.SF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setnz
+static void do_execute(){
+	if(cpu.ZF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr seto
+static void do_execute(){
+	if(cpu.OF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setp
+static void do_execute(){
+	if(cpu.PF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setpe
+static void do_execute(){
+	if(cpu.PF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setpo
+static void do_execute(){
+	if(cpu.PF==0)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr sets
+static void do_execute(){
+	if(cpu.SF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#define instr setz
+static void do_execute(){
+	if(cpu.ZF==1)
+		OPERAND_W(op_src,1);
+	else
+		OPERAND_W(op_src,0);
+	print_asm_template1();
+}
+make_instr_helper(rm)
+#undef instr
+
+#include "cpu/exec/template-end.h"
