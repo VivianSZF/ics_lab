ae8cbebaf733bc6dbde2a22eefc8fdf24add47c5
tracer-ics2016 <tracer@njuics.org>
2016-10-04 08:30:09 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:30:09 up  3:43,  1 user,  load average: 0.00, 0.00, 0.00 7f0e00c861d1128645bdec7611f73fec1fa63b64

---
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jmp
+
+static void do_execute(){
+	int len=instr_len();
+	if(op_src->type==OP_TYPE_IMM){
+		cpu.eip+=op_src->val;
+		print_asm("jmp"str(SUFFIX) " %x",cpu.eip+len+1);
+		}
+	else
+		cpu.eip=op_src->val-len-1;
+}
+
+make_instr_helper(i)
+make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"
