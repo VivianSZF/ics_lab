89fbf4378297b3fe93d1a5e89bf127e3ad106c16
tracer-ics2016 <tracer@njuics.org>
2016-10-04 06:02:52 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  06:02:52 up  2:24,  1 user,  load average: 0.02, 0.03, 0.00 75ab3be5dcf656087d713ef1791b74e9a051ed11

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr stos
+
+static void do_execute(){
+	if(DATA_BYTE==1){
+		swaddr_write(cpu.edi,DATA_BYTE,cpu.gpr[0]._8[0]);
+	}
+	else if(DATA_BYTE==2){
+		swaddr_write(cpu.edi,DATA_BYTE,cpu.gpr[0]._16);
+	}
+	else{
+		swaddr_write(cpu.edi,DATA_BYTE,cpu.eax);
+	}
+	if(cpu.DF==0)
+		cpu.edi+=DATA_BYTE;
+	else
+		cpu.edi-=DATA_BYTE;
+	print_asm_template2();
+}
+
+make_instr_helper(n);
+
+#include "cpu/exec/template-end.h"
