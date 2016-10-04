ad8982c61fc146af6081dfe35f550c0940a2a281
tracer-ics2016 <tracer@njuics.org>
2016-10-04 10:27:05 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:27:05 up  4:39,  1 user,  load average: 0.00, 0.00, 0.00 4a1f7d388b25cefc445e995a222eecad716c681b

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -19,14 +19,15 @@ static void do_execute(){
 	printf("%d\n",len);
 	printf("%d\n",op_src->val);
 	printf("%x\n",cpu.eip);
+	DATA_TYPE_S dis=op_src->val;
 	if(cpu.CF==1||cpu.ZF==1){
-		cpu.eip+=op_src->val;
-		print_asm("jbe %x",cpu.eip+len+1);
+		cpu.eip+=dis;
+		print_asm("jbe %x",cpu.eip+DATA_BYTE+1);
 	}
-	//else
-	//{
-//		print_asm("jbe %x",cpu.eip+op_src->val+len+1);
-//	}
+	else
+	{
+		print_asm("jbe %x",cpu.eip+dis+DATA_BYTE+1);
+	}
 }
 make_instr_helper(i)
 #undef instr
