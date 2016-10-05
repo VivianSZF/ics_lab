d595854728b70eaedbab426866aa894675ac1699
tracer-ics2016 <tracer@njuics.org>
2016-10-05 13:18:28 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:18:28 up 41 min,  1 user,  load average: 0.00, 0.00, 0.00 8830171a657ab6a7606b57549ad78edd17696e61

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -198,7 +198,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x88 */	inv, inv, inv, inv, 
 /* 0x8c */	inv, inv, inv, inv, 
 /* 0x90 */	inv, inv, setb_rm_b, setae_rm_b,
-/* 0x94 */	inv, inv, setbe_rm_b, seta_rm_b,
+/* 0x94 */	inv, setne_rm_b, setbe_rm_b, seta_rm_b,
 /* 0x98 */	inv, inv, inv, inv, 
 /* 0x9c */	inv, inv, inv, inv, 
 /* 0xa0 */	inv, inv, inv, inv, 
