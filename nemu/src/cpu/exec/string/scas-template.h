cd28053180e51213c8d30aae335c991f5ae5649c
tracer-ics2016 <tracer@njuics.org>
2016-10-12 03:04:24 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  03:04:24 up  3:02,  1 user,  load average: 0.20, 0.15, 0.06 52f59fd1997eebb0b1ec4a7cb9c46670acb0a6fc

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
 	op_src->type=op_dest->type=OP_TYPE_REG;
 	op_src->reg=R_EAX;
 	op_dest->reg=R_EDI;
-	/*if(DATA_BYTE==1){
+	if(DATA_BYTE==1){
 		op_src->val=cpu.gpr[0]._8[0];
 	}
 	else if(DATA_BYTE==2){
@@ -14,8 +14,8 @@ static void do_execute(){
 	}
 	else{
 		op_src->val=cpu.eax;
-	}*/
-	op_src->val=REG(R_EAX);
+	}
+	//op_src->val=REG(R_EAX);
 	op_dest->val=swaddr_read(cpu.edi,DATA_BYTE);
 	snprintf(op_src->str,11,"%%es:(%%edi)");
 	snprintf(op_dest->str,5,"%%eax");
