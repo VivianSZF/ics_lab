8b097723ad55259db0b2ff31e30b2807955284c1
tracer-ics2016 <tracer@njuics.org>
2016-10-12 00:09:03 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:09:03 up 6 min,  1 user,  load average: 0.10, 0.45, 0.33 7a86c910507ef0e6e12a5e4327e02a3db5d5c92d

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -3,8 +3,15 @@
 #define instr lods
 
 static void do_execute(){
-	
-	cpu.eax=swaddr_read(cpu.esi,DATA_BYTE);
+	if(DATA_BYTE==1){
+		cpu.gpr[0]._8[0]=swaddr_read(cpu.esi,1);
+	}
+	else if(DATA_BYTE==2){
+		cpu.gpr[0]._16=swaddr_read(cpu.esi,2);
+	}
+	else{
+		cpu.eax=swaddr_read(cpu.esi,4);
+	}
 	//REG(R_EAX)=MEM_R(REG(R_ESI));
 	if(cpu.DF==0)
 		cpu.esi+=DATA_BYTE;
