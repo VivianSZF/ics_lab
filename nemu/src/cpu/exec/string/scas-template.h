14e39c4f7ab5d091cbd4163ac532f0bf9037c6da
tracer-ics2016 <tracer@njuics.org>
2016-10-13 00:15:43 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:15:43 up 17 min,  1 user,  load average: 0.07, 0.13, 0.15 fbfd2b38d513d6d1d0f7b0968f9299d39eec816

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -3,29 +3,30 @@
 #define instr scas
 
 static void do_execute(){
-	op_src->type=op_dest->type=OP_TYPE_REG;
-	op_src->reg=R_EAX;
-	op_dest->reg=R_EDI;
+	//op_src->type=op_dest->type=OP_TYPE_REG;
+	//op_src->reg=R_EAX;
+	//op_dest->reg=R_EDI;
+	DATA_TYPE src;
 	if(DATA_BYTE==1){
-		op_src->val=REG(R_AL);
+		src=REG(R_AL);
 	}
 	else if(DATA_BYTE==2){
-		op_src->val=REG(R_AX);
+		src=REG(R_AX);
 	}
 	else{
-		op_src->val=REG(R_EAX);
+		src=REG(R_EAX);
 	}
 	//op_src->val=REG(R_EAX);
-	op_dest->val=swaddr_read(cpu.edi,DATA_BYTE);
-	snprintf(op_src->str,11,"%%es:(%%edi)");
-	snprintf(op_dest->str,5,"%%eax");
+	//op_dest->val=swaddr_read(cpu.edi,DATA_BYTE);
+	//snprintf(op_src->str,11,"%%es:(%%edi)");
+	//snprintf(op_dest->str,5,"%%eax");
 	//DATA_TYPE src=REG(R_EAX);
 	//DATA_TYPE src=cpu.eax;
-	//DATA_TYPE dest=MEM_R(REG(R_EDI));
+	DATA_TYPE dest=MEM_R(reg_l(R_EDI));
 	//DATA_TYPE dest=swaddr_read(cpu.edi,DATA_BYTE);
-	DATA_TYPE result=op_src->val-op_dest->val;
-	cpu.CF=(op_src->val<op_dest->val);
-	cpu.OF=((MSB(op_src->val)!=MSB(op_dest->val))&&(MSB(result)!=MSB(op_src->val)));
+	DATA_TYPE result=src-dest;
+	cpu.CF=(src<dest);
+	cpu.OF=((MSB(src)!=MSB(dest))&&(MSB(result)!=MSB(src)));
 	if(cpu.DF==0)
 		cpu.edi+=DATA_BYTE;
 	else
