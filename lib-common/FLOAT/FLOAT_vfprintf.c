7ee0aba7626c348ec584a4ba42abec44a92d40a5
tracer-ics2016 <tracer@njuics.org>
2016-10-27 11:48:31 +0000

    >  run 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:48:31 up  2:08,  1 user,  load average: 0.00, 0.01, 0.00 fb00cf81edb0ee9936fb7ebf5355f1c5c7ec104d

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -18,8 +18,10 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	 */
 
 	char buf[80];
-	int len;// = sprintf(buf, "0x%08x", f);
-	len+=sprintf(buf,"%u",f>>16);
+	int len=0;
+	if((f&0x80000000)==0x80000000)
+		len=sprintf(buf,"-");
+	len+=sprintf(buf+len,"%u",f>>16);
 	len+=sprintf(buf+len,".");
 	uint64_t xiaoshu;
 	xiaoshu=(uint64_t)(f&0x0000ffff);
