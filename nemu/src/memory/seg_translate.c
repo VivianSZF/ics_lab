40d100b768bb3b09cbaf694bc89b054a33fd9d2f
tracer-ics2016 <tracer@njuics.org>
2016-11-27 08:26:47 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:26:47 up  5:20,  1 user,  load average: 0.12, 0.04, 0.01 afddf815377fc61f8189ecfa8b07a5cd33747831

--- nemu/src/memory/seg_translate.c
+++ nemu/src/memory/seg_translate.c
@@ -21,6 +21,7 @@ lnaddr_t seg_translate(swaddr_t addr,size_t len, uint8_t sreg){
 		cpu.segcache[sreg].base=base;
 		cpu.segcache[sreg].valid=true;
 	}
+	printf("%x  %x\n",cpu.sreg[sreg].INDEX,cpu.GDTR.base);
 	lnaddr=addr+base;
 	return lnaddr;
 }
