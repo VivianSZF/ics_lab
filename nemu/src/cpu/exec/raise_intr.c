9776db6742132d79b7d9580ba83aaa0c0a152bf6
tracer-ics2016 <tracer@njuics.org>
2016-12-10 16:04:01 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:04:01 up 10:01,  1 user,  load average: 0.00, 0.03, 0.00 aec13b1d7bd96f74b44dfa277d15391a2804bb5

--- nemu/src/cpu/exec/raise_intr.c
+++ nemu/src/cpu/exec/raise_intr.c
@@ -9,18 +9,18 @@ extern jmp_buf jbuf;
 void raise_intr(uint8_t NO,uint32_t len){
 	cpu.esp-=4;
 	swaddr_write(cpu.esp,4,cpu.eflags_val,S_SS);
-	printf("fine1\n");
+//	printf("fine1\n");
 	cpu.esp-=4;
 	swaddr_write(cpu.esp,4,cpu.CS,S_SS);
-	printf("fine2\n");
+//	printf("fine2\n");
 	cpu.esp-=4;
 	swaddr_write(cpu.esp,4,cpu.eip+len,S_SS);
-	printf("fine3\n");
+//	printf("fine3\n");
 	uint8_t t[8];
 	int i;
 	for(i=0;i<8;i++){
 		t[i]=lnaddr_read(cpu.IDTR.base+NO*8+i,1);
-		printf("fine\n");
+//		printf("fine\n");
 	}
 	GateDesc *gate=(GateDesc *)t;
 	cpu.CS=gate->segment;
