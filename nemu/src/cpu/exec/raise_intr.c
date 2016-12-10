407165de672e563f142f4e116f705e8eb0561872
tracer-ics2016 <tracer@njuics.org>
2016-12-10 15:53:26 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:53:26 up  9:50,  1 user,  load average: 0.12, 0.04, 0.01 7a7224442f2dd3353780a00eb017fa66ea0a632b

--- nemu/src/cpu/exec/raise_intr.c
+++ nemu/src/cpu/exec/raise_intr.c
@@ -21,13 +21,13 @@ void raise_intr(uint8_t NO,uint32_t len){
 	GateDesc *gate=(GateDesc *)t;
 	cpu.CS=gate->segment;
 	
-	uint8_t t1[8];
+/*	uint8_t t1[8];
 	for(i=0;i<8;i++)
 		t1[i]=lnaddr_read(cpu.GDTR.base+cpu.sreg[S_CS].INDEX*8+i,1);
 	SegDesc *segd=(SegDesc *)t1;
 	cpu.segcache[S_CS].base=segd->base_15_0+(segd->base_23_16<<16)+(segd->base_31_24<<24);
 	cpu.segcache[S_CS].valid=true;
-
+*/
 	cpu.eip=gate->offset_15_0+(gate->offset_31_16<<16);
 
 	longjmp(jbuf,1);
