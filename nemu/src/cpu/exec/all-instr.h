66df00eab56010ba03602ad1402dc05d80f19f2a
tracer-ics2016 <tracer@njuics.org>
2016-12-14 02:29:07 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  02:29:07 up 28 min,  1 user,  load average: 0.08, 0.03, 0.07 1031694c9ef2da759175c349e76cc298960abad6

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -14,6 +14,7 @@
 #include "data-mov/cld.h"
 #include "data-mov/pusha.h"
 #include "data-mov/popa.h"
+#include "data-mov/in.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
---
+++ nemu/src/cpu/exec/data-mov/in-template.h
@@ -0,0 +1,21 @@
+#include "cpu/exec/template-start.h"
+
+#define instr in
+
+uint32_t pio_read(ioaddr_t,size_t);
+
+make_helper(concat(in_i_,SUFFIX)){
+	uint16_t p=instr_fetch(eip+1,1);
+	REG(R_EAX)=(DATA_TYPE)pio_read(p,DATA_BYTE);
+	print_asm("in");
+	return 2;
+}
+
+make_helper(concat(in_r_,SUFFIX)){
+	uint16_t p=reg_w(R_DX);
+	REG(R_EAX)=(DATA_TYPE)pio_read(p,DATA_BYTE);
+	print_asm("in");
+	return 2;
+}
+
+#include "cpu/exec/template-end.h"
