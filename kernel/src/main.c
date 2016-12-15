3ee093f43c04c606115a79fdda98e15861b93bed
tracer-ics2016 <tracer@njuics.org>
2016-12-15 16:31:38 +0000

    >  compile kernel 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:31:38 up  5:49,  1 user,  load average: 0.03, 0.07, 0.03 a4afbb793cea420e551e5f2ab08cd9abb0c1bc4b

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -71,13 +71,14 @@ void init_cond() {
 	 * Note that the output is actually performed only when
 	 * the serial port is available in NEMU.
 	 */
-	assert(0);
+	
 	Log("Hello, NEMU world!");
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
 	video_mapping_write_test();
 #endif
+	assert(0);
 	/* Load the program. */
 	uint32_t eip = loader();
 	
