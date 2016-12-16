9193ffc920eba065c605d4c62ec8226d396ee546
tracer-ics2016 <tracer@njuics.org>
2016-12-16 01:08:56 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:08:56 up 22 min,  1 user,  load average: 0.08, 0.03, 0.05 9ad9a9ab5879140a8a4ca3ec7347ac6d71d5812

--- nemu/src/cpu/exec/data-mov/in-template.h
+++ nemu/src/cpu/exec/data-mov/in-template.h
@@ -10,9 +10,4 @@ make_helper(concat(in_,SUFFIX)){
 	return 1;
 }
 
-/*make_helper(concat(in_,SUFFIX)){
-		concat(reg_,SUFFIX)(R_EAX)=pio_read(reg_w(R_DX),DATA_BYTE);
-		print_asm("in");
-		return 1;
-		}*/
 #include "cpu/exec/template-end.h"
