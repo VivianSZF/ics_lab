420fac371dfbf7225731d77180b5b6b7129a574c
tracer-ics2016 <tracer@njuics.org>
2016-12-16 01:07:35 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:07:35 up 20 min,  1 user,  load average: 0.00, 0.00, 0.04 608a4d0daa7fe79c754e58877121dc9363a4604c

--- nemu/src/cpu/exec/data-mov/in-template.h
+++ nemu/src/cpu/exec/data-mov/in-template.h
@@ -3,24 +3,16 @@
 #define instr in
 
 uint32_t pio_read(ioaddr_t,size_t);
-/*
-make_helper(concat(in_i_,SUFFIX)){
-	uint16_t p=instr_fetch(eip+1,1);
-	REG(R_EAX)=(DATA_TYPE)pio_read(p,DATA_BYTE);
-	print_asm("in");
-	return 2;
-}
 
-make_helper(concat(in_r_,SUFFIX)){
-	uint16_t p=reg_w(R_DX);
-	REG(R_EAX)=(DATA_TYPE)pio_read(p,DATA_BYTE);
+make_helper(concat(in_,SUFFIX)){
+	REG(R_EAX)=(DATA_TYPE)pio_read(reg_w(R_DX),DATA_BYTE);
 	print_asm("in");
 	return 1;
 }
-*/
-make_helper(concat(in_,SUFFIX)){
+
+/*make_helper(concat(in_,SUFFIX)){
 		concat(reg_,SUFFIX)(R_EAX)=pio_read(reg_w(R_DX),DATA_BYTE);
 		print_asm("in");
 		return 1;
-		}
+		}*/
 #include "cpu/exec/template-end.h"
