4646b7cf317f4bfcaf02d3fc3837f1e60ea56b6f
tracer-ics2016 <tracer@njuics.org>
2016-12-16 00:49:45 +0000

    >  compile NEMU 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:49:45 up 2 min,  1 user,  load average: 0.34, 0.45, 0.20 8f9fa0a6d3be79667642f9a1e5de98154c21d3e

--- nemu/src/device/ide.c
+++ nemu/src/device/ide.c
@@ -76,7 +76,7 @@ void ide_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 
 			byte_cnt += 4;
 			if(byte_cnt == 512) {
-				/* finish */assert(0);
+				/* finish */
 				ide_port_base[7] = 0x40;
 			}
 		}
