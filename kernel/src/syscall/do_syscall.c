d2b8da8b1bdf0fd748bbb964c2b782ccbb3ee2d3
tracer-ics2016 <tracer@njuics.org>
2017-01-08 07:49:13 +0000

    >  compile kernel 151180115 vivian Linux 9a05e0edc522 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  07:49:13 up  8:53,  1 user,  load average: 0.04, 0.03, 0.01 cb842d5d30a57079620c23d5e98b78a20213e2c

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -6,6 +6,12 @@ void add_irq_handle(int, void (*)(void));
 uint32_t mm_brk(uint32_t);
 int fs_ioctl(int, uint32_t, void *);
 void serial_printc(char);
+int fs_open(const char *pathname,int flags);
+int fs_read(int fd,void *buf,int len);
+int fs_write(int fd,void *buf,int len);
+int fs_lseek(int fd,int offset,int whence);
+int fs_close(int fd);
+
 
 static void sys_brk(TrapFrame *tf) {
 	tf->eax = mm_brk(tf->ebx);
@@ -15,7 +21,7 @@ static void sys_ioctl(TrapFrame *tf) {
 	tf->eax = fs_ioctl(tf->ebx, tf->ecx, (void *)tf->edx);
 } 
 
-static void sys_write(TrapFrame *tf) {
+/*static void sys_write(TrapFrame *tf) {
 #ifdef HAS_DEVICE
 	int i;
 	for(i=0;i<tf->edx;i++){
@@ -27,7 +33,7 @@ static void sys_write(TrapFrame *tf) {
 
 	tf->eax=tf->edx;
 }
-
+*/
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
 		/* The `add_irq_handle' system call is artificial. We use it to
@@ -43,7 +49,22 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
-		case SYS_write: sys_write(tf); break;
+		//case SYS_write: sys_write(tf); break;
+		case SYS_write:
+			tf->eax=fs_write(tf->ebx,(void *)tf->ecx,tf->edx);
+			break;
+		case SYS_read:
+			tf->eax=fs_read(tf->ebx,(void *)tf->ecx,tf->edx);
+			break;
+		case SYS_open:
+			tf->eax=fs_open((const char*)tf->ebx,tf->ecx);
+			break;
+		case SYS_lseek:
+			tf->eax=fs_lseek(tf->ebx,tf->ecx,tf->edx);
+			break;
+		case SYS_close:
+			tf->eax=fs_close(tf->ebx);
+			break;
 		/* TODO: Add more system calls. */
 
 		default: panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
